2504f2d3d31903c3fa5b1d1cc3efe82a14d23e3d
Kurumigu <mag65qz@gmail.com>
2026-02-01 15:44:09 +0900

    帰省前の保存

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Cart
+    path('cart/add/', views.AddCartView.as_view()),
+    path('cart/', views.CartListView.as_view()),  # カートページ
+
+    # Items
+    path('items/<str:pk>/', views.ItemDetailView.as_view()),
+
+    path('', views.IndexListView.as_view()), # トップページ
 ]
